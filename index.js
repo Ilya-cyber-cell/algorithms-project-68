074c36020f74a3e65ea86ba6c4c54e20527caff6
Ilya Voronin <voronin.i.a@yandex.ru>
2025-01-26 09:20:51 +0000

    tree

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ import makeRouter from './src/router.js';
 
 
 const routes = [
-    {
-      // Роутер используется как часть на конкретном сайте, поэтому роутеру нужно знать лишь про сами маршруты на сайте
-      // не учитываем протокол, хост и т. д.
-      path: '/courses', // маршрут
-      handler: () => 'courses!', // обработчик
-    },
-    {
-      path: '/courses/basics',
-      handler: () => 'basics',
-    },
-  ];
+  {
+    path: '/courses/:id',
+    handler: () => 'course!',
+  },
+  {
+    path: '/courses/:course_id/exercises/:id',
+    handler: () => 'exercise!',
+  },
+  {
+    path: '/courses/11111/exercises/2222',
+    handler: () => 'exercise!',
+  },
+  {
+    path: '/courses/11111/exercises/:id',
+    handler: () => 'exercise!',
+  },    
+];
+
   const router = makeRouter(routes);
   
-  const path = '/courses';
+//  const path = '/courses/php_trees';
+  const path = '/courses/11111/exercises/3333';
   const handler = router.serve(path);
   console.log(handler);
-  console.log(handler()); // courses!
+//  console.log(handler()); // courses!
   
   // здесь выбросится исключение
-  console.log(router.serve('/no_such_way'));
+//  console.log(router.serve('/no_such_way'));
 
--- src/router.js
+++ src/router.js
@@ -1,20 +1,84 @@
+class Trie {
+  constructor(key, parent = null) {
+      this.key = key;
+      this.children = {};
+      this.parent = parent;
+      this.handler = ""
+      this.end = false;
+  }
+
+  getWord() {
+      let output = [];
+      let node = this;
+
+      while (node !== null) {
+          output.unshift(node.key);
+          node = node.parent;
+      }
+      return output.join('');
+  }
+  insert(path, handler) {
+    let node = this;
+    for (let i = 0; i < path.length; i++) {
+        if (!node.children[path[i]]) {
+            node.children[path[i]] = new Trie(path[i], node);
+        }
+        node = node.children[path[i]];
+        if (i === path.length - 1) {
+            node.end = true;
+            node.handler = handler
+        }
+    }
+  }
+  contains(path) {
+    let node = this;
+    let params = new Array()
+    let handler = ""
+    for (let i = 0; i < path.length; i++) {
+        let param = -1
+        for (let key in node.children){
+          if (key[0] == ":"){
+            param = key
+            params[key] = path[i]
+          }
+        }
+        if (node.children[path[i]] ) {
+            node = node.children[path[i]];
+        } else if( param != -1 ){
+            node = node.children[param]
+        } else {
+            return [false, params, handler];
+        }
+    }
+    handler = node.handler 
+    return [true, params, handler]
+  }  
+}
+
 class Router {
   router;
-  paths
+  paths;
+  prefix_tree;
   constructor(router) {
-    this.router = router;
+    this.router = []
     this.paths = []
+    let prefix_tree = new Trie()
+    for (const  i in router){
+      prefix_tree.insert(router[i]['path'].split('/').splice(1), router[i]['handler']);
+      this.prefix_tree = prefix_tree;
+    }
   }
   serve(path){
     console.log("============")
-    let ret = 0
-    for (const  i in this.router){
-      if (this.router[i]['path'] == path) {
-        ret = this.router[i]['handler']
-      }
+    let ret = ""
+    let path_spited = path.split('/').splice(1)
+    let prefix_tree = this.prefix_tree
+    const [path_found, params, handler] =  prefix_tree.contains(path_spited)
+    if ( !path_found ) {
+      return  new Error("Path not found");
     }
-//    const obj = Object.fromEntries(ret);
-    return Function(ret)
+    ret = {'path':path, 'handler':handler, 'params':params };
+    return ret
   }
 }
 
